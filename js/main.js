26d873244f34b8e4a07cb0e582bc1f83a862489f
nextjenart <58447826+nextjenart@users.noreply.github.com>
2019-12-03 11:27:53 -0800

    Have to force these through.
    Git keeps adding random text into my files, but I've gone in and manually deleted them. Let's see what happens.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 /* This is my main JS file */
+
+.calc-grid-column(@index, @class, @type) when (@type = width) and (@index > 0) {
+  .col-@{class}-@{index} {
+    width: percentage((@index / @grid-columns));
+  }
+}
