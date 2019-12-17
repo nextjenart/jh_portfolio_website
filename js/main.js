162ea7cc9d5b6a1a0a28771276ed07e814b9f9ce
nextjenart <58447826+nextjenart@users.noreply.github.com>
2019-12-16 17:42:07 -0800

    Updated to reflect hamburger menu changes.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
 
 console.log("Wait, you weren't supposed to see this! Shoo! SHOO!");
 
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
